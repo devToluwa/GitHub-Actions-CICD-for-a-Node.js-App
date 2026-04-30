9addb839c5108b62808620558cef7ee6a4050c75
Toluwa Oloba <toluwadev@outlook.com>
2026-04-30 16:09:12 +0100

    add easter egg 👀

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD pipeline project')
+  // Sending HTML instead of plain text
+  res.send('Hello from CI/CD pipeline project, <a href="https://youtu.be/dQw4w9WgXcQ">click me to vie repo</a>');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
