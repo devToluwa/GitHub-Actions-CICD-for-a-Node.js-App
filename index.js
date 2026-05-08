55ad6d63640e93e9afb956daf76a877c47efd5de
Toluwa Oloba <toluwadev@outlook.com>
2026-05-08 08:39:30 +0100

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   // Sending HTML instead of plain text
-  res.send('Hello from CI/CD pipeline project, <a href="https://youtu.be/dQw4w9WgXcQ">click me to vie repo</a>');
+  res.send('<a href="https://youtu.be/dQw4w9WgXcQ">Hello from CI/CD pipeline project</a>');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
